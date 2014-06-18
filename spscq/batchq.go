a9f9ce7dc19f6f50806a704c60b185ffccf2eb31
Francis Stephens <francisstephens@gmail.com>
2014-06-18 14:51:51 +0100

    head/tail indices not in fatomic.Padded64Int64
    Previously we held each of our head/tail and head/tailCache indices in
    their own cache line padded structures to prevent false sharing. Now
    they are separated into blocks of ordinary int64 values protected by
    unused cacheline padding.
    
    This appears to have no performance impact but is a significant
    improvement in code clarity.

--- spscq/batchq.go
+++ spscq/batchq.go
@@ -9,16 +9,17 @@ import (
 
 type BatchQ struct {
 	_1        fatomic.Padded64Int64
-	head      fatomic.Padded64Int64
-	headCache fatomic.Padded64Int64
-	tail      fatomic.Padded64Int64
-	tailCache fatomic.Padded64Int64
+	head      int64
+	tailCache int64
 	_2        fatomic.Padded64Int64
+	tail      int64
+	headCache int64
+	_3        fatomic.Padded64Int64
 	// Read only
 	ringBuffer []unsafe.Pointer
 	size       int64
 	mask       int64
-	_3         fatomic.Padded64Int64
+	_4         fatomic.Padded64Int64
 }
 
 func NewBatchQ(size int64) *BatchQ {
@@ -31,40 +32,40 @@ func NewBatchQ(size int64) *BatchQ {
 }
 
 func (q *BatchQ) WriteBuffer(bufferSize int64) []unsafe.Pointer {
-	tail := q.tail.Value
+	tail := q.tail
 	idx := tail & q.mask
 	bufferSize = min(bufferSize, q.size-idx)
 	writeTo := tail + bufferSize
 	headLimit := writeTo - q.size
 	nxt := idx + bufferSize
-	if headLimit > q.headCache.Value {
-		q.headCache.Value = atomic.LoadInt64(&q.head.Value)
-		if headLimit > q.headCache.Value {
-			nxt = q.headCache.Value &q.mask
+	if headLimit > q.headCache {
+		q.headCache = atomic.LoadInt64(&q.head)
+		if headLimit > q.headCache {
+			nxt = q.headCache &q.mask
 		}
 	}
 	return q.ringBuffer[idx:nxt]
 }
 
 func (q *BatchQ) CommitWrite(writeSize int64) {
-	atomic.StoreInt64(&q.tail.Value, q.tail.Value+writeSize)
+	atomic.StoreInt64(&q.tail, q.tail+writeSize)
 }
 
 func (q *BatchQ) ReadBuffer(bufferSize int64) []unsafe.Pointer {
-	head := q.head.Value
+	head := q.head
 	idx := head & q.mask
 	bufferSize = min(bufferSize, q.size-idx)
 	readTo := head + bufferSize
 	nxt := idx + bufferSize
-	if readTo > q.tailCache.Value {
-		q.tailCache.Value = atomic.LoadInt64(&q.tail.Value)
-		if readTo > q.tailCache.Value {
-			nxt = q.tailCache.Value & q.mask
+	if readTo > q.tailCache {
+		q.tailCache = atomic.LoadInt64(&q.tail)
+		if readTo > q.tailCache {
+			nxt = q.tailCache & q.mask
 		}
 	}
 	return q.ringBuffer[idx:nxt]
 }
 
 func (q *BatchQ) CommitRead(readSize int64) {
-	atomic.StoreInt64(&q.head.Value, q.head.Value+readSize)
+	atomic.StoreInt64(&q.head, q.head+readSize)
 }
